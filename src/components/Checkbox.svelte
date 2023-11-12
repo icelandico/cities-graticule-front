8feb6c829b94ac0fb597e21a8c5a0cf6524dc871
Michal <hi@geodev.me>
2023-11-12 15:51:45 +0100

    Move Map to separate component, create separate Checkbox component

---
+++ src/components/Checkbox.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import Option from './../App.svelte';
+  export let value: string;
+  export let isChecked: boolean;
+  export let label: string;
+  export let handleChange: (val: Option) => void;
+</script>
+
+<label class="option-container">
+  {label}
+  <input type="checkbox" bind:checked={isChecked} on:change={() => handleChange(value)}>
+  <span class="checkmark"></span>
+</label>
+
+<style>
+
+.option-container {
+    display: block;
+    position: relative;
+    margin-bottom: 12px;
+    cursor: pointer;
+    font-size: 18px;
+    user-select: none;
+  }
+
+  .option-container input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 25px;
+    width: 25px;
+    background-color: #eee;
+  }
+
+  .option-container:hover input ~ .checkmark {
+    background-color: #ccc;
+  }
+
+  .option-container input:checked ~ .checkmark {
+    background-color: #2196F3;
+  }
+
+  .checkmark:after {
+    content: "";
+    position: absolute;
+    display: none;
+  }
+
+  .option-container input:checked ~ .checkmark:after {
+    display: block;
+  }
+
+  .option-container .checkmark:after {
+    left: 9px;
+    top: 5px;
+    width: 5px;
+    height: 10px;
+    border: solid white;
+    border-width: 0 3px 3px 0;
+    transform: rotate(45deg);
+  }
+</style>
---
+++ src/components/Map.svelte
@@ -0,0 +1,41 @@
+<script>
+  import * as L from 'leaflet';
+  import { Geocoder, geocoders } from 'leaflet-control-geocoder';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    const leafletMap = L.map('map').setView([54.364917, 18.422872], 3);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      minZoom: 0,
+      maxZoom: 20,
+      maxNativeZoom: 19,
+      attribution: '© OpenStreetMap contributors',
+    }).addTo(leafletMap);
+
+    new Geocoder({
+      geocoder: new geocoders.Nominatim(),
+      position: 'topright',
+    }).on('markgeocode', function (e) {
+      const { name, center } = e.geocode;
+      console.log(`Found location: ${name}, Coordinates: ${center}`);
+    }).addTo(leafletMap);
+  });
+</script>
+
+<div class="map-container">
+  <div id="map"></div>
+</div>
+
+<style>
+  .map-container {
+    border-radius: 8px;
+    height: 500px;
+    margin-bottom: 2rem;
+  }
+  #map {
+    height: 500px;
+    border-radius: 8px;
+    margin-bottom: 2rem;
+  }
+</style>
